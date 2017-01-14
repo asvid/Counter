414e45b3a7644eac1ed0f215d3706253cd3bb3f8
adam <adam.swiderski89@gmail.com>
2017-01-15 00:00:52 +0100

    WIP adds basic widget files

---
+++ app/src/main/java/widget/BeerCounterWidget.java
@@ -0,0 +1,16 @@
+package widget;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by adam on 14.01.17.
+ */
+
+public class BeerCounterWidget extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}
---
+++ app/src/main/java/widget/BeerCounterWidgetConfigure.java
@@ -0,0 +1,8 @@
+package widget;
+
+/**
+ * Created by adam on 14.01.17.
+ */
+
+public class BeerCounterWidgetConfigure {
+}
